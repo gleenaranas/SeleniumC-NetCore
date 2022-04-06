186d375e2d576b2412965ebf35d9e76310a199fd
gleenaranas <gleenaranas@gmail.com>
2022-04-06 11:07:20 +0800

    Added cw "sample update for github in UnitTest

--- AutomationTesting/UnitTest1.cs
+++ AutomationTesting/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 namespace AutomationTesting
 {
@@ -22,10 +23,11 @@ namespace AutomationTesting
             //CustomControl control = new CustomControl(); - will need to remove this if driverhelper is static
             //Set value for ComboBox
             CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
+
+
+
             
-
-
-
+            Console.WriteLine("Sample update for github");
 
             /*
              login CRM
