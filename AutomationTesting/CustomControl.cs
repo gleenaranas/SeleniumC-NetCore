3dd194661ef8a22f06310d78e8e8a4c7de1e4206
gleenaranas <gleenaranas@gmail.com>
2022-04-06 10:44:29 +0800

    Initial commit until combo controls
    Initial commit until combo controls

---
+++ AutomationTesting/CustomControl.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTesting
+{
+    public class CustomControl : DriverHelper
+    {
+        //Set parameter for ComboBox method
+        public static void ComboBox(String controlName, String value)
+        {
+            //Create new label for finding control element
+            IWebElement comboControl = Driver.FindElement(By.Id($"{controlName}-awed"));
+            comboControl.Clear();
+            comboControl.SendKeys(value);
+
+            Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']")).Click();
+        }
+    }
+}
---
+++ AutomationTesting/UnitTest1.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTesting
+{
+    public class Tests: DriverHelper
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com");
+
+            //Initialize new object to CustomControl class
+            //CustomControl control = new CustomControl(); - will need to remove this if driverhelper is static
+            //Set value for ComboBox
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
+            
+
+
+
+
+            /*
+             login CRM
+             Driver.FindElement(By.Id("i0116")).SendKeys("GAranas@lowyinstitute.org");
+             Driver.FindElement(By.Id("idSIButton9")).Click();
+             Driver.FindElement(By.Id("i0118")).SendKeys("ShortRock99");
+             Driver.FindElement(By.Id("idSIButton9")).Click(); 
+              Driver.Quit
+            */
+
+        }
+    }
+}
